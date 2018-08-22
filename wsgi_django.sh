572b1b3660543c924377f41d7381c129f432734b
Anton Bozhina <38840879+anton-bozhina@users.noreply.github.com>
2018-08-22 23:12:28 +0300

    Update wsgi_django.sh

--- wsgi_django.sh
+++ wsgi_django.sh
@@ -46,11 +46,11 @@ RewriteCond %{HTTP_HOST} ^www.$2\.ru\$ [NC]
 RewriteRule ^(.*)\$ http://$2/\$1 [R=301,L]
 
 RewriteCond %{REQUEST_FILENAME} !-f
-RewriteRule ^(.*)\$ /app/wsgi.py/\$1 [QSA,PT,L]" > $docroot/.htaccess
+RewriteRule ^(.*)\$ /app/app.wsgi/\$1 [QSA,PT,L]" > $docroot/.htaccess
 chown $user:$user $docroot/.htaccess
 fi
 
-echo "touch $docroot/app/wsgi.py" > $docroot/touch.sh
+echo "touch $docroot/app/app.wsgi" > $docroot/touch.sh
 chown $user:$user $docroot/touch.sh
 chmod +x $docroot/touch.sh
 
@@ -58,6 +58,6 @@ echo "For install requirements packs:
 cd $home_dir/$user/web/$domain/private/; source venv/bin/activate; pip install -r $docroot/requirements.txt; deactivate
 
 For reload app:
-touch $docroot/app/wsgi.py" > $docroot/help
+touch $docroot/app/app.wsgi" > $docroot/help
 
 exit 0
